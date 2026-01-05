632c2954028545be4a4d0a9188f22581d580c4da
tech-sirka <tech@s-i-r-k-a.com>
2026-01-05 22:42:51 +0200

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -13,18 +13,15 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
+    // Always add console transport so logs appear in PM2
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
   ],
 });
-
-// Add console transport in development
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    )
-  }));
-}
 
 module.exports = logger;
 
